594f29dd713415b9d03839ed1ed0cd587c2c6a02
Dron07Stm12 <dronperenen@gmail.com>
2023-03-15 09:46:46 +0200

    индексаторы и их реализация через IEnumerable и IEnumerator

--- Program.cs
+++ Program.cs
@@ -1,44 +1,94 @@
 ﻿using System;
+using System.Collections;
 
 namespace Console_Class
 {
 
     class Person
     {
-      /*public*/  string name;
-      /*public*/  int age;
+        public string Name { get; set; }
+        //public Person(string name) => Name = name;
 
-        public Person(string name) : base()
-        {
-            this.name = name;
-            Console.WriteLine("Person(string name)" + this.name);
-        }
-        public Person(string name, int age) :this(name) 
-        {
-            this.age = age;
-            Console.WriteLine("Person(string name, int age)" + name + age);
-        }
+        public Person(string name) { Name = name; }
     }
-
-    class Employee : Person
+    class Company /*: IEnumerable*/
     {
-        string company;
+        Person[] personal;
 
-        public Employee(string name, int age, string company) : base(name, age)
+        public Company(Person[] people)
         {
-            this.company = company;
-            Console.WriteLine("Employee(string name, int age, string company)"+ name + age + company);
+                personal= people;
         }
+
+        //public Company(Person[] people) => personal = people;
+        // индексатор
+        //public Person this[int index]
+        //{
+        //    get => personal[index];
+        //    set => personal[index] = value;
+        //}
+
+        //Индексаторы позволяют индексировать объекты и обращаться к данным по индексу.
+        //Фактически с помощью индексаторов мы можем работать с объектами как с массивами.
+        public Person this[int index]
+        {
+            get { return personal[index]; }
+            set { personal[index] = value; }
+        
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return personal.GetEnumerator();
+
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator() 
+        //{ return personal.GetEnumerator(); }
     }
-
-
 
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee tom = new Employee("Tom", 22, "Microsoft");
+            Company company = new Company(new Person[] { new Person("Dron"), new Person("Lero"),new Person("Nata") });
+            Console.WriteLine(company[0].Name);
+            Person person = company[1]; 
+            Console.WriteLine(person.Name);
+            Console.WriteLine("***");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(company[i].Name);
+            }
+            Console.WriteLine("***");
+            foreach (Person item in company)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("***");
+            foreach (var item in company)
+            {
+                Console.WriteLine(((Person)item).Name);
+            }
+            Console.WriteLine("***");
+
+            IEnumerator enumerator = company.GetEnumerator();
+
+            while (enumerator.MoveNext()) {
+
+                Console.WriteLine(((Person)enumerator.Current).Name);
+            }
+            Console.WriteLine("***");
+
+
+            company[0] = new Person("Kampot");
+            Console.WriteLine(company[0].Name);
+
+            person.Name = "Belka";
+            company[2].Name = person.Name;
+            Console.WriteLine(company[2].Name);
+
         }
     }
 }
