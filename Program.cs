e084cf862e6b51d72c37f91654c3f44bbc51446f
Dron07Stm12 <dronperenen@gmail.com>
2023-03-15 12:55:35 +0200

    Нисходящие преобразования. Downcasting(от  базовому  к производного)

--- Program.cs
+++ Program.cs
@@ -45,7 +45,9 @@ namespace Console_Class
     {
         static void Main(string[] args)
         {
-            //Восходящие преобразования. Upcasting
+            //Восходящие преобразования. Upcasting(от производного к базовому)
+
+
             //Объекты производного типа (который находится внизу иерархии) в то же время представляют
             //и базовый тип. Например, объект Employee в то же время является и объектом класса Person. 
             Employee employee = new Employee("Tom", "Microsoft");
@@ -63,9 +65,66 @@ namespace Console_Class
 
             Console.WriteLine(person.Name +" works of"+ employee.Company);
 
-            //происходит неявное восходящее преобразование
+            //происходит неявное восходящее преобразование(потомучто Client производный от Person), но видимость или доступ
+            //ссылки person1  будет только к функционалу базового класса т.е. к классу Person
             Person person1 = new Client("Dron","CrediAg");
-            Console.WriteLine(person1.Name.GetHashCode());
+            Console.WriteLine(person1.Name);
+
+
+            object obj = new Client("Bob", "ContosoBank");  // от Client к object
+            Console.WriteLine(((Client)obj).Name + " works " + ((Client)obj).Bank);
+
+
+            //Нисходящие преобразования. Downcasting(от  базовому  к производного)
+
+
+            //вопрос, можно ли обратиться к функционалу типа Employee через переменную типа Person.
+            //Но автоматически такие преобразования не проходят, ведь не каждый человек (объект Person)
+            //является сотрудником предприятия (объектом Employee). И для нисходящего преобразования
+            //необходимо применить явное преобразование, указав в скобках тип, к которому нужно выполнить преобразование:
+
+            Employee employee1 = new Employee("Tom", "Microsoft");
+            Person person2 = employee1;   // преобразование от Employee к Person
+            //а вот наоборот нельзя
+            //Employee employee2 = person;    // так нельзя, нужно явное преобразование
+            Employee employee2 = (Employee)person2;  // преобразование(явное) от Person к Employee
+
+            object obj2 = new Employee("Lero","Home");
+
+            // не можем положить обьект базового класса в ссылку производного, без явного приведения
+            Employee employee3 = (Employee)obj2;
+            Console.WriteLine(employee3.Name+" sitting of "+employee3.Company);
+            //или
+            Console.WriteLine(((Employee)obj2).Name +" "+ ((Employee)obj2).Company);
+
+            // объект Client также представляет тип Person
+            Person person3 = new Client("Sam", "ContosoBank");
+            //производим нисходящее явное преобразование от  Person к  Client
+            Client client = (Client)person3;
+            Console.WriteLine(client.Name);
+            Console.WriteLine(client.Bank);
+
+
+
+            // Объект Employee также представляет тип object
+            object obj3 = new Employee("Bill", "Microsoft");
+
+            // преобразование к типу Client, чтобы получить свойство Bank
+            // не будет так как Client не является обьектом Employee
+            //string bank = ((Client)obj3).Bank;
+
+            Person person4 = new Person("Bob");
+            Employee employee4 = (Employee)person4; // ! Ошибка
+
+            //В данном случае мы пытаемся преобразовать объект типа Person к типу Employee,
+            //а объект Person не является объектом Employee. Причем в последнем случае Visual Studio не подскжет,
+            //что в данной строке ошибка, и данная строка даже нормально скомилируется, тем не менее в процессе
+            //выполнения программы мы получим ощибку. В этом в том числе и кроектся коварство преобразований,
+            //поэтому в подобных ситуациях надо проявлять осторожность.
+
+
+
+
 
 
         }
