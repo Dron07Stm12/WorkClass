5cde2e03564ca0e7a3638cd8197f7aad12561d9b
Dron07Stm12 <dronperenen@gmail.com>
2023-03-16 09:37:49 +0200

    индексатор и реализация метода GetEnumerator()

--- Program.cs
+++ Program.cs
@@ -3,41 +3,49 @@ using System.Collections;
 
 namespace Console_Class
 {
-    class Person
+    //Индексаторы позволяют индексировать объекты и обращаться к данным по индексу.
+    //Фактически с помощью индексаторов мы можем работать с объектами как с массивами.
+    //По форме они напоминают свойства со стандартными блоками get и set,
+    //которые возвращают и присваивают значение.
+
+
+    class Person 
     {
-        public string Name { get; set; }
-        public Person(string name) : base()
+      public string Name { get; set; }
+        public Person(string name)
         {
-            Name = name;
-        }
-        public void Print()
-        {
-            Console.WriteLine($"Person {Name}");
-        }
-
-
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
-        }
+                Name= name;
+           
+        } 
+    
+    
     }
 
-    class Employee : Person
+    class Company : IEnumerable
     {
-        public string Company { get; set; }
-        public Employee(string name, string company) : base(name)
-        {
-            Company = company;
-        }
-    }
+        private Person[] person1;
 
-    class Client : Person
-    {
-        public string Bank { get; set; }
-        public Client(string name, string bank) : base(name)
+        public Company(Person[] people)
         {
-            Bank = bank;
+            person1 = people;
         }
+
+         public  Person this[int index] {
+
+            get { return person1[index]; }
+            set { person1[index] = value; } 
+        
+         }
+
+        // просто реализация метода  GetEnumerator()
+        //public IEnumerator GetEnumerator()
+        //{
+        //    return person1.GetEnumerator();
+
+        //}
+        //реализация интерфейса IEnumerable и метода GetEnumerator() явным образом
+        IEnumerator IEnumerable.GetEnumerator() { return person1.GetEnumerator(); }
+
     }
 
 
@@ -45,34 +53,58 @@ namespace Console_Class
     {
         static void Main(string[] args)
         {
-            //оператор as
-            //Person person = new Person("Tom");
-            //Employee employee = person as Employee;
-            //if (employee == null)
-            //{
-            //    Console.WriteLine("Преобразование прошло неудачно");
-            //}
-            //else { employee.Print(); }
+            Company company = new Company(new Person[] {new Person("Dron"),new Person("Lero"),new Person("Nata") } );
 
-            //оператор is
-            Person person2 = new Person("Tom");
-            if (person2 is Employee employee )
+            Person person = company[0];
+            Console.WriteLine(person.Name);
+            // можно использовать ключевое слово as. С помощью него программа пытается преобразовать
+            //выражение к определенному типу, при этом не выбрасывает исключение. В случае неудачного преобразования
+            //выражение будет содержать значение null:
+            Person person1 = company[0] as Person;
+
+            if (person1 == null)
             {
-                Console.WriteLine(employee.Name);
+                Console.WriteLine("Преобразование прошло неудачно");
             }
-
             else
             {
-                Console.WriteLine("Преобразование не допустимо");
+                Console.WriteLine(person1.Name + " as");
+               
             }
 
-
-            Employee employee1 = new Employee("Dron","Nargus");
-
-            if (employee1 is Person person3)
+            string s = company[0].Name as string;
+            if (s != null)
             {
-                person3.Print();
+                Console.WriteLine(s +" as");
             }
+
+           
+
+
+
+
+
+            Console.WriteLine("***");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(company[i].Name);
+            }
+            Console.WriteLine("*** also foreach");
+
+            foreach (Person item in company)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("*** also IEnumerator");
+            //IEnumerator enumerator = company.GetEnumerator();
+            //while (enumerator.MoveNext()) 
+            //{
+            //    Console.WriteLine(((Person)enumerator.Current).Name);
+            
+            //}
+            //enumerator.Reset(); 
         }
     }
 }
