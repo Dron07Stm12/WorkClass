ce4c795856a67b85de5d36306635403ea4240a67
Dron07Stm12 <dronperenen@gmail.com>
2023-03-24 14:45:17 +0200

    update Console_Class.csproj

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections;
+using System.Net.Http.Headers;
 
 namespace Console_Class
 {
@@ -38,21 +40,97 @@ namespace Console_Class
          }
 
         // просто реализация метода  GetEnumerator()
-        //public IEnumerator GetEnumerator()
-        //{
-        //    return person1.GetEnumerator();
+        public IEnumerator GetEnumerator()
+        {
+            return person1.GetEnumerator();
 
-        //}
+        }
         //реализация интерфейса IEnumerable и метода GetEnumerator() явным образом
-        IEnumerator IEnumerable.GetEnumerator() { return person1.GetEnumerator(); }
+        //IEnumerator IEnumerable.GetEnumerator() { return person1.GetEnumerator(); }
 
     }
+
+
+
+    class User
+    {
+        //public string Name
+        //{
+        //    set { name = value; }
+        //    get { return name; }
+
+        //}
+        string name = "";
+        string email = "";
+        string phone = "";
+
+        public string this[string s] 
+        {
+
+            get {
+
+                switch (s)
+                {
+                    case "name": return name;  
+                        
+                    case "email":   return email;
+
+                    case "phone": return phone;
+                        default: throw new Exception("Unknown Property Name - неверно");
+                           
+                }
+
+
+
+            }
+
+            set { 
+                switch (s) 
+                {
+                        case"name": name = value; 
+                        break;
+                        case "email": email = value;    
+                        break;
+                        case "phone": phone = value;
+                        break;  
+                
+                
+                }
+            
+            
+            
+            }   
+        
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            StringValues strings2 = "1";
+            StringValues strings3 = "0";
+            string s5 = StringValues.Concat(strings2, strings3);
+            Console.WriteLine(StringValues.Concat(strings2, strings3));
+
+
+
+
             Company company = new Company(new Person[] {new Person("Dron"),new Person("Lero"),new Person("Nata") } );
 
             Person person = company[0];
@@ -98,13 +176,42 @@ namespace Console_Class
             }
 
             Console.WriteLine("*** also IEnumerator");
-            //IEnumerator enumerator = company.GetEnumerator();
-            //while (enumerator.MoveNext()) 
-            //{
-            //    Console.WriteLine(((Person)enumerator.Current).Name);
-            
-            //}
-            //enumerator.Reset(); 
+            IEnumerator enumerator = company.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(((Person)enumerator.Current).Name);
+
+            }
+            enumerator.Reset();
+
+            //индексы необязательно должны представлять тип int, устанавливаемые/возвращаемые значения
+            //необязательно хранить в массиве. Например, мы можем рассматривать объект как хранилище атрибутов/свойств и
+            //передавать имя атрибута в виде строки
+            //В данном случае индексатор в классе User в качестве индекса получает строку,
+            //которая хранит название атрибута (в данном случае название поля класса).
+
+
+            //Мы работаем с обьектом типа User со ссылкой user
+            User user = new User();
+
+            //так как у этого обьекта(ссылки user) есть 3 string поля,
+            //а индексатор( возвращаемым тип также string) и индексом string,
+            // то User в качестве индекса получает строку,
+            //которая хранит название атрибута (в данном случае название поля класса).
+            user["name"] = "Tom";
+            Console.WriteLine(user["name"]);
+
+            string str = user["name"] as string;
+            if (str != null) { Console.WriteLine(str + " as"); }
+            else
+            {
+                Console.WriteLine("Преобразование не корректно");
+            }
+            user["email"] = "D";
+            string str2 = user["email"];
+            Console.WriteLine(str2);
+
+
         }
     }
 }
